c7a6b92d19b9c6333ec3ce556c678aaef22c42f9
Knevari <mateus7319@gmail.com>
2018-11-06 13:34:24 -0300

    adicionado arquivo de configuração padrão do tema

--- views/login.js
+++ views/login.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 import LoginForm from '../components/LoginForm.js';
 
-import { View, StyleSheet } from 'react-native';
+import theme from '../theme/theme.json';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar
+} from 'react-native';
 
 const login = props => {
 
   return (
     <View style={styles.view}>
-
-      <LoginForm />
-
+      <StatusBar
+        backgroundColor={ theme.statusbar.default_color } />
+      <Text>Teste</Text>
     </View>
   )
 
@@ -21,9 +28,12 @@ const login = props => {
 const styles = StyleSheet.create({
 
   view: {
-    display:        'flex',
-    textAlign:      'center',
-    justifyContent: 'center',
+    display         : 'flex',
+    textAlign       : 'center',
+    justifyContent  : 'center',
+    alignItems      : 'center',
+    height          : '100%',
+    backgroundColor : theme.background.light_green
   }
 
 })
